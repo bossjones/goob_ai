0af6e15d17a5c2153dcc9c1ed39a1b2fb96b2e82
Malcolm Jones <bossjones@theblacktonystark.com>
2024-05-04 19:16:04 -0400

    chg: cleanup bot

--- src/goob_ai/aio_settings.py
+++ src/goob_ai/aio_settings.py
@@ -1,7 +1,9 @@
 """aio_settings"""
 
 # pylint: disable=no-name-in-module
-# type: ignore[call-arg
+# type: ignore[call-arg]
+# type: ignore[attr-defined]
+# type: ignore[pydantic-field]
 from __future__ import annotations
 
 from typing import Any, Callable, List, cast
@@ -26,14 +28,12 @@ from pathlib import Path
 from tempfile import gettempdir
 from typing import Any, Dict, Optional
 
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic.functional_validators import field_validator
 from rich.console import Console
 from rich.table import Table
 from yarl import URL
 
-# import tilda
 
 TEMP_DIR = Path(gettempdir())
 
@@ -185,22 +185,22 @@ class AioSettings(BaseSettings):
     """
 
     # main directory to place models in etc eg. ~/cerebro
-    cerebro_root: str = "~/cerebro"
+    # cerebro_root: str = "~/cerebro"
     monitor_host: str = "localhost"
-    monitor_port: int = 50101
+    monitor_port: int = 50102
 
     # tweetpik_background_image = "510"  # a image that you want to use as background. you need to use this as a valid url like https://mysite.com/image.png and it should not be protected by cors
-    audit_log_send_channel = ""
+    audit_log_send_channel: str = ""
 
     # ***************************************************
     # NOTE: these are grouped together
     # ***************************************************
     token: str = ""
-    prefix: str = "%"
+    prefix: str = "/"
     # default_config = {"token": "", "prefix": constants.prefix}
     # ***************************************************
 
-    default_dropbox_folder: str = "/cerebro_downloads"
+    # default_dropbox_folder: str = "/cerebro_downloads"
     # default_tweetpik_options = {}
     discord_admin_user_id: str = ""
     # TODO: ^ change this ->  to ^  and find all instances where it is called. discord_admin = os.environ.get("discord_admin_user_id")
@@ -211,134 +211,30 @@ class AioSettings(BaseSettings):
 
     # discord_server_id: Optional[str] = None
     discord_token: str = ""
-    dropbox_cerebro_app_key: str = ""
-    dropbox_cerebro_app_secret: str = ""
-    dropbox_cerebro_token: str = ""
-    tweetpik_authorization: str = ""
-    # change the background color of the tweet screenshot
-    tweetpik_background_color: str = "#ffffff"
-    tweetpik_bucket_id: str = "323251495115948625"
-
-    # any number higher than zero. this value is used in pixels(px) units
-    tweetpik_canvas_width: str = "510"
-    tweetpik_dimension_ig_feed: str = "1:1"
-    tweetpik_dimension_ig_story: str = "9:16"
-    tweetpik_display_likes: bool = False
-    tweetpik_display_link_preview: bool = True
-    tweetpik_display_media_images: bool = True
-    tweetpik_display_replies: bool = False
-    tweetpik_display_retweets: bool = False
-    tweetpik_display_source: bool = True
-    tweetpik_display_time: bool = True
-    tweetpik_display_verified: bool = True
-
-    # change the link colors used for the links, hashtags and mentions
-    tweetpik_link_color: str = "#1b95e0"
-
-    tweetpik_text_primary_color: str = (
-        "#000000"  # change the text primary color used for the main text of the tweet and user's name
-    )
-    tweetpik_text_secondary_color: str = (
-        "#5b7083"  # change the text secondary used for the secondary info of the tweet like the username
-    )
-
-    # any number higher than zero. this value is representing a percentage
-    tweetpik_text_width: str = "100"
-
-    tweetpik_timezone: str = "america/new_york"
-
-    # change the verified icon color
-    tweetpik_verified_icon: str = "#1b95e0"
-
-    globals_root = cerebro_root
-
-    # Where to look for the initialization file
-    globals_initfile = "cerebro.init"
-    globals_models_file = "models.yaml"
-    globals_models_dir = "ml_models"
-    globals_config_dir = "configs"
-    globals_autoscan_dir = "weights"
-    globals_converted_ckpts_dir = "converted_ckpts"
 
     # Try loading patchmatch
-    globals_try_patchmatch = True
+    globals_try_patchmatch: bool = True
 
     # Use CPU even if GPU is available (main use case is for debugging MPS issues)
-    globals_always_use_cpu = False
+    globals_always_use_cpu: bool = False
 
     # Whether the internet is reachable for dynamic downloads
     # The CLI will test connectivity at startup time.
-    globals_internet_available = True
+    globals_internet_available: bool = True
 
     # whether we are forcing full precision
-    globals_full_precision = False
+    globals_full_precision: bool = False
 
     # whether we should convert ckpt files into diffusers models on the fly
-    globals_ckpt_convert = False
+    globals_ckpt_convert: bool = False
 
     # logging tokenization everywhere
-    globals_log_tokenization = False
-
-    gallery_dl_config_dir: str = "~/.config/gallery-dl"
-    gallery_dl_homedir_config: str = "~/.gallery-dl.conf"
-    netrc_config_filepath: str = "~/.netrc"
-
-    yt_dl_config_dir: str = "~/Downloads"
-
-    # TODO: Convert all of these to settings
-    # DISCORD_SERVER_ID
-    # DROPBOX_GOOB_AI_APP_KEY = os.environ.get("DROPBOX_GOOB_AI_APP_KEY")
-    # DROPBOX_GOOB_AI_APP_SECRET = os.environ.get("DROPBOX_GOOB_AI_APP_SECRET")
-
-    # DROPBOX_GOOB_AI_TOKEN = os.environ.get("DROPBOX_GOOB_AI_TOKEN")
-    # DEFAULT_DROPBOX_FOLDER = "/cerebro_downloads"
-
-    # DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
-    # default_config = {"token": "", "prefix": constants.PREFIX}
-    # AUDIT_LOG_SEND_CHANNEL = os.environ.get("AUDIT_LOG_SEND_CHANNEL")
-
-    # TWEETPIK_AUTHORIZATION = os.environ.get("TWEETPIK_AUTHORIZATION")
-    # TWEETPIK_BUCKET_ID = "323251495115948625"
-
-    # TWEETPIK_DIMENSION_IG_FEED = "1:1"
-    # TWEETPIK_DIMENSION_IG_STORY = "9:16"
-    # TWEETPIK_TIMEZONE = "America/New_York"
-    # TWEETPIK_DISPLAY_LIKES = False
-    # TWEETPIK_DISPLAY_REPLIES = False
-    # TWEETPIK_DISPLAY_RETWEETS = False
-    # TWEETPIK_DISPLAY_VERIFIED = True
-    # TWEETPIK_DISPLAY_SOURCE = True
-    # TWEETPIK_DISPLAY_TIME = True
-    # TWEETPIK_DISPLAY_MEDIA_IMAGES = True
-    # TWEETPIK_DISPLAY_LINK_PREVIEW = True
-    # TWEETPIK_TEXT_WIDTH = (
-    #     "100"  # Any number higher than zero. This value is representing a percentage
-    # )
-    # TWEETPIK_CANVAS_WIDTH = (
-    #     "510"  # Any number higher than zero. This value is used in pixels(px) units
-    # )
-    # # TWEETPIK_BACKGROUND_IMAGE = "510"  # A image that you want to use as background. You need to use this as a valid URL like https://mysite.com/image.png and it should not be protected by CORS
-
-    # TWEETPIK_BACKGROUND_COLOR = (
-    #     "#FFFFFF"  # Change the background color of the tweet screenshot
-    # )
-    # TWEETPIK_TEXT_PRIMARY_COLOR = "#000000"  # Change the text primary color used for the main text of the tweet and user's name
-    # TWEETPIK_TEXT_SECONDARY_COLOR = "#5B7083"  # Change the text secondary used for the secondary info of the tweet like the username
-    # TWEETPIK_LINK_COLOR = (
-    #     "#1B95E0"  # Change the link colors used for the links, hashtags and mentions
-    # )
-    # TWEETPIK_VERIFIED_ICON = "#1B95E0"  # Change the verified icon color
-
-    # DEFAULT_TWEETPIK_OPTIONS = {}
-
-    # DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
-    # DISCORD_ADMIN = os.environ.get("DISCORD_ADMIN_USER_ID")
-    # DISCORD_GUILD = os.environ.get("DISCORD_SERVER_ID")
-    # DISCORD_GENERAL_CHANNEL = 908894727779258390
+    globals_log_tokenization: bool = False
 
     # ************************************************************************
 
-    log_level: LogLevel = LogLevel.DEBUG
+    # log_level: LogLevel = LogLevel.DEBUG.value
+    # log_level
 
     # Variables for Redis
     redis_host: str = "localhost"
@@ -346,20 +242,6 @@ class AioSettings(BaseSettings):
     redis_user: Optional[str] = None
     redis_pass: Optional[str] = None
     redis_base: Optional[int] = None
-
-    # field_validators
-    _normalize_cerebro_root = field_validator("cerebro_root", allow_reuse=True)(normalize_settings_path)
-    _normalize_gallery_dl_config_dir = field_validator("gallery_dl_config_dir", allow_reuse=True)(
-        normalize_settings_path
-    )
-    _normalize_gallery_dl_homedir_config = field_validator("gallery_dl_homedir_config", allow_reuse=True)(
-        normalize_settings_path
-    )
-    _normalize_netrc_config_filepath = field_validator("netrc_config_filepath", allow_reuse=True)(
-        normalize_settings_path
-    )
-    _normalize_yt_dl_config_dir = field_validator("yt_dl_config_dir", allow_reuse=True)(normalize_settings_path)
-    _normalize_globals_root = field_validator("globals_root", allow_reuse=True)(normalize_settings_path)
 
     @property
     def redis_url(self) -> URL:
@@ -387,157 +269,10 @@ class AioSettings(BaseSettings):
         """
         return {"port": self.monitor_port, "host": self.monitor_host}
 
-    @property
-    def global_config_file(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.globals_root, self.globals_config_dir, self.globals_models_file)
-
-    @property
-    def global_config_dir(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.globals_root, self.globals_config_dir)
-
-    @property
-    def global_models_dir(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.globals_root, self.globals_models_dir)
-
-    @property
-    def global_autoscan_dir(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.globals_root, self.globals_autoscan_dir)
-
-    @property
-    def global_converted_ckpts_dir(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.global_models_dir, self.globals_converted_ckpts_dir)
-
-    @property
-    def esgran_dir(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.global_models_dir, "esgran")
-
-    @property
-    def screencropnet_dir(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.global_models_dir, "screencropnet")
-
-    @property
-    def screencropnet_model_fpath(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.screencropnet_dir, "ScreenCropNetV1_378_epochs.pth")
-
-    @property
-    def gallery_dl_config_dot_json(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.gallery_dl_config_dir, "config.json")
-
-    @property
-    def yt_dlp_config(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.yt_dl_config_dir, "yt-cookies.txt")
-
-    @property
-    def netrc_config(self) -> Path:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return Path(self.netrc_config_filepath)
-
-    @property
-    def gallery_dl_cookie_files(self) -> Dict[str, Path]:
-        """_summary_
-
-        Returns:
-            Path: _description_
-        """
-        return {
-            "instagram": Path(self.gallery_dl_config_dir, "cookies-instagram.txt"),
-            "pinterest": Path(self.gallery_dl_config_dir, "cookies-pinterest.txt"),
-            "reddit": Path(self.gallery_dl_config_dir, "cookies-reddit.txt"),
-            "redgifs": Path(self.gallery_dl_config_dir, "cookies-redgifs.txt"),
-            "twitter": Path(self.gallery_dl_config_dir, "cookies-twitter.txt"),
-            "default_cookies": Path(self.gallery_dl_config_dir, "cookies.txt"),
-            "hlm_ig": Path(self.gallery_dl_config_dir, "hlm-cookies-instagram.txt"),
-            "reactionmemestv_ig": Path(self.gallery_dl_config_dir, "reactionmemestv-cookies-instagram.txt"),
-            "universityofprofessorex_ig": Path(
-                self.gallery_dl_config_dir, "universityofprofessorex-cookies-instagram.txt"
-            ),
-            "wavy_ig": Path(self.gallery_dl_config_dir, "wavy-cookies-instagram.txt"),
-        }
-
     class Config:  # sourcery skip: docstrings-for-classes
         env_file = ".env"
-        env_prefix = "CEREBROBOT_CONFIG_"
+        env_prefix = "GOOB_AI_"
         env_file_encoding = "utf-8"
 
 
 aiosettings = AioSettings()  # sourcery skip: docstrings-for-classes, avoid-global-variables
-
-# Need to create a command that can audit and make sure all of these are available.
-# pi@boss-deeplearning ~/dev/just master*
-# ❯ tree ~/.config/gallery-dl/
-# /home/pi/.config/gallery-dl/
-# ├── config.json -> /home/pi/.gallery-dl.conf
-# ├── cookies-instagram.txt
-# ├── cookies-pinterest.txt
-# ├── cookies-reddit.txt
-# ├── cookies-redgifs.txt
-# ├── cookies-twitter.txt
-# ├── cookies.txt
-# ├── cookies.txt.bak
-# ├── hlm-cookies-instagram.txt
-# ├── reactionmemestv-cookies-instagram.txt
-# ├── universityofprofessorex-cookies-instagram.txt
-# └── wavy-cookies-instagram.txt
-
-# /home/pi/.netrc
-# ls -lta ~/.gallery-dl.conf
-
-# pi@boss-deeplearning ~/dev/just master*
-# ❯ ls -lta ~/Downloads/yt-cookies.txt
-# -rw-r--r-- 1 pi pi 9626 Mar  4 16:42 /home/pi/Downloads/yt-cookies.txt
-# -rw-r--r-- 1 pi pi 9626 Mar  4 16:42 /home/pi/Downloads/yt-cookies.txt
-# -rw-r--r-- 1 pi pi 9626 Mar  4 16:42 /home/pi/Downloads/yt-cookies.txt
