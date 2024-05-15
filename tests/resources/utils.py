161433a5e299aff176abbae57ee31b9ab0f70f3a
Malcolm Jones <bossjones@theblacktonystark.com>
2024-05-15 07:26:18 -0400

    chg: pillow fixes

--- tests/resources/utils.py
+++ tests/resources/utils.py
@@ -6,6 +6,7 @@ from warnings import warn
 
 import PIL
 
+from PIL import ImageChops
 from PIL.Image import Image
 
 
@@ -26,12 +27,12 @@ def images_are_identical(image_1: Image, image_2: Image):
         warn(f"Images do not have the same mode. Found {image_1.mode} and {image_2.mode}.")
         return False
     elif image_1.mode in ["1", "L"]:
-        return PIL.ImageChops.difference(image_1, image_2).getextrema() == ((0, 0))
+        return ImageChops.difference(image_1, image_2).getextrema() == ((0, 0))
     elif image_1.mode == "LA":
-        return PIL.ImageChops.difference(image_1, image_2).getextrema() == ((0, 0), (0, 0))
+        return ImageChops.difference(image_1, image_2).getextrema() == ((0, 0), (0, 0))
     elif image_1.mode == "RGB":
-        return PIL.ImageChops.difference(image_1, image_2).getextrema() == ((0, 0), (0, 0), (0, 0))
+        return ImageChops.difference(image_1, image_2).getextrema() == ((0, 0), (0, 0), (0, 0))
     elif image_1.mode == "RGBA":
-        return PIL.ImageChops.difference(image_1, image_2).getextrema() == ((0, 0), (0, 0), (0, 0), (0, 0))
+        return ImageChops.difference(image_1, image_2).getextrema() == ((0, 0), (0, 0), (0, 0), (0, 0))
     else:
         raise NotImplementedError(f"Unable to compare images of type {image_1.mode}.")
