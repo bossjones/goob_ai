0aa1370a4b2a8cbd5f89ba4408c63f7d6056fe30
Malcolm Jones <bossjones@theblacktonystark.com>
2024-06-08 23:11:17 -0400

    Add Any type hint to the imports in imgops.py.

--- src/goob_ai/utils/imgops.py
+++ src/goob_ai/utils/imgops.py
@@ -459,7 +459,7 @@ import traceback
 import typing
 
 from enum import IntEnum
-from typing import Dict, List, NewType, Optional, Tuple
+from typing import Dict, List, NewType, Optional, Tuple, Any
 
 import cv2
 import numpy as np
